0fa8429a1033f8e5b6b4a5141d96a3649b753e3a
Lovly <youssef@xatoma.com>
2024-07-08 18:13:44 +0100

    .

--- config.example.js
+++ config.example.js
@@ -53,7 +53,7 @@ module.exports = {
     // Logging Level (error, warn, info, verbose, debug, silly)
     'logLevel': 'debug',
     // Max amount of simultaneous requests from the same IP  (incl. WS and HTTP/HTTPS), -1 for unlimited
-    'max_simultaneous_requests': 1,
+    'max_simultaneous_requests': -1,
     // Bool to enable game file updates from the SteamDB Github tracker (updated item definitions, images, names)
     'enable_game_file_updates': true,
     // Amount of seconds to wait between updating game files (0 = No Interval Updates)
@@ -61,7 +61,7 @@ module.exports = {
     // Postgres connection string to store results in (ex. postgres://user:pass@127.0.0.1:5432/postgres?sslmode=disable)
     'database_url': '',
     // OPTIONAL: Enable bulk inserts, may improve performance with many requests
-    'enable_bulk_inserts': false,
+    'enable_bulk_inserts': true,
     // OPTIONAL: Key by the caller to allow inserting price information, required to use the feature
     'price_key': '',
     // OPTIONAL: Key by the caller to allow placing bulk searches
